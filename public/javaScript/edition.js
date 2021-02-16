ba0eab5f17bef311b418ed7753eff736e0aeef80
FacuCarbon <facundocarbon2015@gmail.com>
2021-02-16 16:03:27 -0300

    algun cambio

--- public/javaScript/edition.js
+++ public/javaScript/edition.js
@@ -1,9 +1,47 @@
-let iten = document.querySelector(".iten")
-let isResizer = false;
 // Funcion para mover la imagen
+function empezar(e){
+  e.dataTransfer.setData('Text', e.target.src);
+  e.dataTransfer.effectAllowed = 'move';
+}
+  
+function moverImagen(e) {
+  e.dataTransfer.dropEffect = 'move';
+  return false;
+}
+
+function soltar (e) {
+  e.preventDefault();
+  imagen = new Image();
+  imagen.src = e.dataTransfer.getData('Text');
+  imagen.style.width = "100%"
+  //document.getElementById('item').appendChild(imagen);
+  document.querySelector('.plantilla').appendChild(imagen);
+}
+  
+//document.getElementById('plantilla').ondragover = moverImagen;
+document.querySelector('.plantilla').ondragover = moverImagen;
+document.getElementById('mover').ondragstart = empezar;
+//document.getElementById('item').ondrop = soltar;
+document.querySelector('.plantilla').ondrop = soltar;
+  
+
+// Ocultar div photos
+function showHide(){
+  var ocultar=document.getElementById('div-invisible') 
+  ocultar.style.display=(ocultar.style.display=='none')?'inline':'none' } 
+
+  //
+  // Ocultar div videos
+function showVideo(){
+  var ocultar=document.getElementById('div-video') 
+  ocultar.style.display=(ocultar.style.display=='none')?'inline':'none' } 
 
 
 // Funcion para agrandar y disminuir
+
+let iten = document.querySelector(".iten")
+let isResizer = false;
+
 const resizers = document.querySelectorAll(".resizer")
 let currentResizer;
 for (const resizer of resizers) {
